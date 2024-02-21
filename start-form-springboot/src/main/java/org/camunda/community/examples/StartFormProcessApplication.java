f503ba41ca0508fd940f8a8ed39ff030c779f528
Varshith Reddy <aleti.varshith@gmail.com>
2024-02-21 15:55:18 -0600

    changes

--- start-form-springboot/src/main/java/org/camunda/community/examples/StartFormProcessApplication.java
+++ start-form-springboot/src/main/java/org/camunda/community/examples/StartFormProcessApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableZeebeClient
+//@EnableZeebeClient
 @Deployment(resources = "classpath*:/model/*.*")
 public class StartFormProcessApplication {
 
--- start-form-springboot/src/main/java/org/camunda/community/examples/StartFormRestController.java
+++ start-form-springboot/src/main/java/org/camunda/community/examples/StartFormRestController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api")
 public class StartFormRestController {
 
   private static final Logger LOG = LoggerFactory.getLogger(StartFormRestController.class);
